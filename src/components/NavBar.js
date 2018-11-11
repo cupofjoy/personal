ab3470a0f4a0c3bfaf3a1275b7a25dd32ff3236b
cupofjoy <eelantung@gmail.com>
2018-11-10 19:39:07 -0500

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import {  Menu } from 'semantic-ui-react'
 
 const NavBar = (props) => {
   return (
-    <Menu fluid centered middle top borderless tabular widths={3}>
+    <div id="navbar" className="ui fluid icon large pointed three item borderless top fixed secondary menu">
       <Link className="ui inverted item" to="/" > Home </Link>
       <Link className="ui inverted item" to="/about" > About </Link>
       {/* <Link className="ui inverted item" to="/blog" > Blog </Link> */}
       <Link className="ui inverted item" to="/contact" > Contact & Links </Link>
-    </Menu>
+    </div>
   )
 }
 
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
+<<<<<<< HEAD
+import { Header } from 'semantic-ui-react'
+=======
+// import { Image } from 'semantic-ui-react'
+>>>>>>> e280ca43745d21ae55791265f5e50a87b5fcee3d
 import '../App.css';
-import { Header, Segment } from 'semantic-ui-react'
 
-/*
-<div className="welcome" style={{paddingTop: '150px', height: '60em', opacity: '0.9'}}><br/>
-  <h2>
-    <i area-hidden="true" className="code teal large icon" />
-    <i area-hidden="true" className="camera grey large icon" />
-    <i area-hidden="true" className="coffee blue large icon" />
-  </h2><br/>
-  <h2 className="ui icon center aligned header">
-    <div id="welcome" className="content">Welcome !</div>
-  </h2>
-</div>
-*/
-
-const WelcomePage = () => {
+const WelcomePage = (props) => {
   return (
-      <Header  as='h4' fluid top id="welcome" style={{ opacity: '0.9' }}><br/>
-          <i area-hidden="true" className="code teal small icon" />
-          <i area-hidden="true" className="camera grey small icon" />
-          <i area-hidden="true" className="coffee blue small icon" />
-          <br/>
-        Welcome !
-      </Header>
+<<<<<<< HEAD
+    <Header className="welcome" style={{paddingTop: '150px', opacity: '0.9'}}><br/>
+=======
+    <div className="welcome" style={{paddingTop: '150px', height: '60em', opacity: '0.9'}}><br/>
+>>>>>>> e280ca43745d21ae55791265f5e50a87b5fcee3d
+      <h2>
+        <i area-hidden="true" className="code teal large icon" />
+        <i area-hidden="true" className="camera grey large icon" />
+        <i area-hidden="true" className="coffee blue large icon" />
+      </h2><br/>
+      <h2 className="ui icon center aligned header">
+        <div id="welcome" className="content">Welcome !</div>
+      </h2>
+<<<<<<< HEAD
+    </Header>
+=======
+    </div>
+>>>>>>> e280ca43745d21ae55791265f5e50a87b5fcee3d
   )
 }
 
