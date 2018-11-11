62e732481d7a2139b7f75c04283d81f6d82287ff
cupofjoy <eelantung@gmail.com>
2018-11-10 19:32:50 -0500

    Revert "edited css, added twitter timeline"
    This reverts commit 32efda7ed8baf04860164e9a2ffc914eb2a427ae.

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../App.css';
-import { Header } from 'semantic-ui-react'
+import { Header, Segment } from 'semantic-ui-react'
+
+/*
+<div className="welcome" style={{paddingTop: '150px', height: '60em', opacity: '0.9'}}><br/>
+  <h2>
+    <i area-hidden="true" className="code teal large icon" />
+    <i area-hidden="true" className="camera grey large icon" />
+    <i area-hidden="true" className="coffee blue large icon" />
+  </h2><br/>
+  <h2 className="ui icon center aligned header">
+    <div id="welcome" className="content">Welcome !</div>
+  </h2>
+</div>
+*/
 
 const WelcomePage = () => {
   return (
